06d1cbb83c9a37c48ede76cec38357e791fdfeb3
ryan <ryan.patel21@imperial.ac.uk>
2023-06-27 13:19:46 +0100

    added hr line after home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
+import getStyles from "./Style";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <hr className={getStyles(styles, "hr")} />
     </>
   );
 }
+
+const styles = {
+  hr: ["m-14", "h-px", "bg-gray-200", "md:hidden"],
+};
