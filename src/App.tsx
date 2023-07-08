ff668213432853b0a8c453909d28c741174d5df0
ryan <ryan.patel21@imperial.ac.uk>
2023-07-08 20:24:30 +0300

    modifieddirectory name for the about section to be consistent with the file naming convention of other directories. Also removed unecessary import getStyles in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import getStyles from "./Style";
-
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import HomeAbout from "./components/homeAbout/HomeAbout";
 import LineBreakLight from "./components/util/LineBreakLight";
-import About from "./components/About/About";
+import About from "./components/about/About";
 
 export default function App() {
   return (
