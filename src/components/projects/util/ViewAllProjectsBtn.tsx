ca3c5b05c01f8bb04977e3489984806c8c910f6d
ryan <ryan.patel21@imperial.ac.uk>
2023-07-28 19:41:29 +0100

    updated view all projects button with hashlink to scroll to top on click

--- src/components/projects/util/ViewAllProjectsBtn.tsx
+++ src/components/projects/util/ViewAllProjectsBtn.tsx
@@ -1,10 +1,10 @@
 import getStyles from "../../../Style";
-import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 function ViewAll() {
   return (
     <section className={getStyles(styles, "ctn")}>
-      <Link to="/projects" className={getStyles(styles, "link")}>
+      <HashLink to="/projects#" className={getStyles(styles, "link")}>
         <span className={getStyles(styles, "text")}>View All</span>
         <svg
           className={getStyles(styles, "icon")}
@@ -13,7 +13,7 @@ function ViewAll() {
         >
           <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
         </svg>
-      </Link>
+      </HashLink>
     </section>
   );
 }
