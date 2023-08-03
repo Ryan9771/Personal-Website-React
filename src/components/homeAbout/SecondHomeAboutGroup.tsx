3672329d5a061055ae3c3fcf1da9673afd8a5d43
ryan <ryan.patel21@imperial.ac.uk>
2023-08-03 10:55:47 +0100

    Fixed responsive margin issues on home-about section

--- src/components/homeAbout/SecondHomeAboutGroup.tsx
+++ src/components/homeAbout/SecondHomeAboutGroup.tsx
@@ -39,7 +39,7 @@ function SecondHomeAboutGroup() {
         </svg>
         <span>Hiking, Nature</span>
       </span>
-      <a
+      {/* <a
         target="_blank"
         href="https://medium.com/@ryanpatel2001"
         className="home-about-icons"
@@ -54,13 +54,23 @@ function SecondHomeAboutGroup() {
         <span>
           <u>Blogging</u>
         </span>
-      </a>
+      </a> */}
+      <span className="home-about-icons">
+        <svg
+          className="home-about-icon-img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z" />
+        </svg>
+        <span>EdTech Software Engineer Intern</span>
+      </span>
     </div>
   );
 }
 
 const styles = {
-  ctn: ["mdl:absolute", "mdl:right-32", "mdl:top-12"],
+  ctn: ["mdl:absolute", "mdl:right-16", "mdl:top-12"],
 };
 
 export default SecondHomeAboutGroup;
